dee7878876d7ea94568d7beb5c05d9c24f0f2b74
Junliang HU <jlhu@cse.cuhk.edu.hk>
2021-09-02 00:19:10 +0800

    Add a data migration test case from PMEM to DRAM

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <oneapi/tbb.h>
+
+#include <execution>
+
+#include "pmutils.hpp"
+
+constexpr auto POOL_SIZE = 8ul << 30;
+
+int main() {
+    // pmutils::cpubind(12);
+    void *drampool[] = {
+        numa_alloc_onnode(POOL_SIZE, 0),
+        numa_alloc_onnode(POOL_SIZE, 1),
+        numa_alloc_onnode(POOL_SIZE, 2),
+        numa_alloc_onnode(POOL_SIZE, 3),
+    };
+    for (auto i = 0ul; i < 4; ++i) {
+        auto dram = drampool[i];
+        tbb::parallel_for(tbb::blocked_range{0ul, POOL_SIZE}, [&](auto &r) {
+            auto begin = dram + r.begin();
+            pmutils::prefault(begin, r.size(), 4ul << 10);
+        });
+    }
+    void *pmempool[] = {
+        pmutils::open("/mnt/pmem0/jlhu/random-testing-0", POOL_SIZE),
+        pmutils::open("/mnt/pmem1/jlhu/random-testing-0", POOL_SIZE),
+        pmutils::open("/mnt/pmem2/jlhu/random-testing-0", POOL_SIZE),
+        pmutils::open("/mnt/pmem3/jlhu/random-testing-0", POOL_SIZE),
+    };
+    for (auto i = 0ul; i < 4; ++i) {
+        auto pmem = pmempool[i];
+        tbb::parallel_for(tbb::blocked_range{0ul, POOL_SIZE}, [&](auto &r) {
+            auto begin = pmem + r.begin();
+            pmem_memset_persist(begin, 'x', r.size());
+        });
+    }
+
+    for (auto node = 0ul; node < 4; ++node) {
+        auto dram = drampool[node];
+        auto actual_node = pmutils::numa_node_of(dram);
+        fmt::print("dram pool {} ptr {} node {} expected {}\n", node, dram,
+                   actual_node, node);
+    }
+
+    for (auto i = 0ul; i < 4; ++i) {
+        auto dram = drampool[i];
+        for (auto j = 0ul; j < 4; ++j) {
+            auto pmem = pmempool[j];
+            // single thread
+            // auto t = pmutils::time([&] { memcpy(dram, pmem, POOL_SIZE); });
+            // multi thread
+            auto t = pmutils::time([&] {
+                tbb::parallel_for(
+                    tbb::blocked_range{0ul, POOL_SIZE}, [&](auto &r) {
+                        memcpy(dram + r.begin(), pmem + r.begin(), r.size());
+                    });
+            });
+            spdlog::info("DRAM {} <- PMEM {} speed {:.4f} GiB/s", i, j,
+                         (double)POOL_SIZE / t.count());
+        }
+    }
+
+    std::for_each(drampool, drampool + 4,
+                  std::bind(numa_free, std::placeholders::_1, POOL_SIZE));
+    return 0;
+}
