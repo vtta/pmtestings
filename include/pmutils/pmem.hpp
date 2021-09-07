c8ecb347ee4a016bfa521d6a2e5cf67aabf9223d
Junliang HU <jlhu@cse.cuhk.edu.hk>
2021-09-07 17:31:36 +0800

    Restructure pmutils sources

---
+++ include/pmutils/pmem.hpp
@@ -0,0 +1,32 @@
+#include <filesystem>
+
+#include "libpmem.h"
+
+namespace pmutils {
+
+inline void *open(char const *path, size_t len) {
+    void *ret = nullptr;
+
+    size_t mapped_len;
+    int is_pmem;
+    if (std::filesystem::exists(path)) {
+        fmt::print("PM pool {} exists\n", path);
+        if (nullptr == (ret = pmem_map_file(path, 0, PMEM_FILE_EXCL, 0666,
+                                            &mapped_len, &is_pmem))) {
+            throw std::runtime_error(pmem_errormsg());
+        }
+    } else {
+        fmt::print("PM pool {} created\n", path);
+        if (nullptr ==
+            (ret = pmem_map_file(path, len, PMEM_FILE_CREATE | PMEM_FILE_EXCL,
+                                 0666, &mapped_len, &is_pmem))) {
+            throw std::runtime_error(pmem_errormsg());
+        }
+        // only need to do this once
+        prefault(ret, len);
+    }
+
+    return ret;
+}
+
+}  // namespace pmutils
