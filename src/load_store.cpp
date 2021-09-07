20d0dbf643a913b6cce71a2aa42a4482066d2caf
Junliang HU <jlhu@cse.cuhk.edu.hk>
2021-09-07 17:32:22 +0800

    Add tests for load/store actions

---
+++ src/load_store.cpp
@@ -0,0 +1,39 @@
+#include <immintrin.h>
+
+#include <concepts>
+#include <iostream>
+
+#include "pmutils.hpp"
+using namespace pmutils;
+
+int main() {
+    int8_t i8;
+    int16_t i16;
+    int32_t i32;
+    int64_t i64;
+    __m128i i128;
+    __m256i i256;
+    __m512i i512;
+
+    __m128i v128 = {128, 128};
+    __m256i v256 = {256, 256, 256, 256};
+    __m512i v512 = {512, 512, 512, 512, 512, 512, 512, 512};
+
+    store(&i8, 8);
+    store(&i16, 16);
+    store(&i32, 32);
+    store(&i64, 64);
+    store(&i128, v128);
+    store(&i256, v256);
+    store(&i512, v512);
+
+    assert(load(&i8) == 8);
+    assert(load(&i16) == 16);
+    assert(load(&i32) == 32);
+    assert(load(&i64) == 64);
+    // assert(load(&i128) == v128);
+    // assert(load(&i256) == v256);
+    // assert(load(&i512) == v512);
+
+    return 0;
+}
